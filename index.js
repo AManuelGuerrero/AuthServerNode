0b7cff1adf2daeeeed31e1e7dadd609ebc21c50a
Antonio Manuel Guerrero Trujillo <antonio.guerrero@atmira.com>
2021-08-04 09:50:57 +0200

    Modificacion seccion 26

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config(); 
 
@@ -25,6 +26,10 @@ app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
 
+//Manejar demás rutas  o en Angular Ruter useHash (Ahora mismo está así)
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'public/index.html'));
+// })
 
 app.listen(process.env.PORT,()=>{
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
